5e416e6db98a282bb0e5e8deea5f9aa0fcd14bf3
Krazune <krazune@gmail.com>
2020-08-06 21:19:14 +0100

    Add perlin noise generator

---
+++ source/perlin_noise_generator.h
@@ -0,0 +1,8 @@
+#ifndef PERLIN_NOISE_GENERATOR_H
+#define PERLIN_NOISE_GENERATOR_H
+
+#include "float_matrix.h"
+
+FloatMatrix *generatePerlinNoiseMatrix(int width, int height, int cellSize);
+
+#endif
